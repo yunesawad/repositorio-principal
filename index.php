690b6fbe8969875f5bf287911f7cdfdd43de7628
Yunis <yunesawad05@gmail.com>
2023-02-02 09:55:00 -0300

    third commit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 echo "<strong>funciona<strong>";
-echo "<br>By Awad's Corporation"
+echo "<br>By Awad's Corporation";
+echo '<br><a href="http://www.consoft.com.py">Visite nosso site</a>';
 ?>
